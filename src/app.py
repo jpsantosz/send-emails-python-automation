a0e1c73882a3ffba44703eea5f82f07f8262604c
jpsantosz <jpsantosc1503@gmail.com>
2024-12-17 11:49:22 -0300

    mudanças no funcionamento do código

--- src/app.py
+++ src/app.py
@@ -1,31 +1,43 @@
 import pandas as pd
 import smtplib
 import os
-import ssl
 import email.message
+import time
 
+# Carrega os e-mails do CSV
 data = pd.read_csv("../data/external/teste-emails.csv")
+emails = data["email"]
 
-email = data["email"][0]
-
-def send_email():
+def send_email(destinatario):
     corpo_email = """
     <p>Olá, isso é um teste!</p>
-    <p><lalalalalalalala/p>
+    <p>lalalalalalalala</p>
     """
+    # Configura mensagem
     msg = email.message.Message()
     msg['Subject'] = 'Email automático'
-    msg['From'] = email
-    msg['To'] = email
-    password = senha
-    msg.add_header('Content_Type', 'text/html')
+    remetente = 'seuemail@gmail.com'  # Seu e-mail
+    msg['From'] = remetente
+    msg['To'] = destinatario
+    password = os.getenv('EMAIL_PASSWORD')  # Variável de ambiente para a senha
+    msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email)
-    
-    s = smtplib.SMTP('smtp.gemail.com: 587')
-    s.starttls()
-    
-    s.login(msg['From'],password)
-    s.sendmail(msg['From'], msg['To'], msg.as_string().encode('utf-8'))
-    print('Email Enviado')
-    
-send_email()
+
+    try:
+        # Configura o servidor SMTP
+        s = smtplib.SMTP('smtp.gmail.com:587')
+        s.starttls()
+        s.login(remetente, password)
+        
+        # Envia o e-mail
+        s.sendmail(remetente, destinatario, msg.as_string().encode('utf-8'))
+        print(f'E-mail enviado com sucesso para: {destinatario}')
+    except Exception as e:
+        print(f"Erro ao enviar e-mail para {destinatario}: {e}")
+    finally:
+        s.quit()
+
+# Loop para enviar o e-mail para todos os destinatários
+for email in emails:
+    send_email(email)
+    time.sleep(2)
